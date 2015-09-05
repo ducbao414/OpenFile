274587021bdc2e10ec7d64c4b80d23f2217541d4
ducbao414 <md231@MacBook-Air.local>
2015-09-05 22:11:53 +0700

    Initial Commit

---
+++ OpenFile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenFile
+//
+//  Created by bmbhh on 9/5/15.
+//  Copyright (c) 2015 bmbhh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
